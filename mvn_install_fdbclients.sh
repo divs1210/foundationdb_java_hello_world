9cba86e756679084a254eb50c6556dcbfdd5e823
Doug Turnbull <dturnbull@o19s.com>
2013-05-06 21:52:08 -0400

    add script to mvn install file the fdb client jars

---
+++ mvn_install_fdbclients.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+FDB_CLIENT_JAR=fdb-java-0.2.1-2-linux-x64.jar
+FDB_JAVADOC_JAR=fdb-java-0.2.1-3-javadoc.jar
+
+mvn install:install-file -Dfile=$FDB_CLIENT_JAR -DgroupId=com.foundationdb \
+        -DartifactId=fdbclient -Djavadoc=$FDB_JAVADOC_JAR \
+        -Dversion=0.2.1 -Dpackaging=jar
